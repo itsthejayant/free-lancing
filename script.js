98f7ecb1c60f8541c3cf0dddb45c3b29f03ed29b
Jayant <jayantaggarwal55@gmail.com>
2023-10-15 23:35:36 +0530

    franchise addes

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('.b');
+
+function startShaking() {
+    button.classList.add('shaking');
+    setTimeout(function() {
+      button.classList.remove('shaking');
+    }, 300);
+    setTimeout(startShaking, 4000); 
+  }
+  
+setTimeout(startShaking, 3000);
