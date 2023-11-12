6d64a045e5335e11d0c00af1a5fa33bd266eb983
Jayant <jayantaggarwal55@gmail.com>
2023-11-12 17:35:32 +0530

    info and aboutus added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const button = document.querySelector('.b');
+// const button = document.querySelector('.b');
 
-function startShaking() {
-    button.classList.add('shaking');
-    setTimeout(function() {
-      button.classList.remove('shaking');
-    }, 300);
-    setTimeout(startShaking, 4000); 
-  }
+// function startShaking() {
+//     button.classList.add('shaking');
+//     setTimeout(function() {
+//       button.classList.remove('shaking');
+//     }, 300);
+//     setTimeout(startShaking, 4000); 
+//   }
   
-setTimeout(startShaking, 3000);
+// setTimeout(startShaking, 3000);
